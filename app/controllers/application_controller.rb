0a5120754e59a929defb3d12124ef7cd90c4ceeb
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-10-12 12:02:26 +0000

    [Fix]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # =======検索窓用(ransack)記述========
   before_action :search
 
+  private
+
   def search
     @q = Souvenir.ransack(params[:q])
   end
