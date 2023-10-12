7e7e12a1ee1c39d02a2dd2eb73b330473147d53f
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-10-12 21:10:49 +0900

    Revert "User側　ransack導入"

--- config/routes.rb
+++ config/routes.rb
@@ -21,19 +21,14 @@ Rails.application.routes.draw do
     get 'users/review' => 'users#review', as: 'user_review'
     get 'users/want' => 'users#want', as: 'user_want'
 
-
-    get 'search/souvenirs' => 'souvenirs#search_souvenir', as: 'search_souvenirs'
-
     get 'search/prefectures/:id' => 'souvenirs#search_prefecture', as: 'search_prefecture'
-    get 'search/categorys' => 'souvenirs#search_category', as: 'search_category'
+    get 'search/category' => 'souvenirs#search_category', as: 'search_category'
+    get 'search/souvenir' => 'souvnirs#search_souvenir', as: 'search_souvenirs'
 
-
-
-    resources :souvenirs do
+    resources :souvenirs, only: [:index, :show, :new, :create] do
       resource :wants, only: [:create, :destroy]
       resources :reviews, except: [:index]
     end
-
 
   end
 
