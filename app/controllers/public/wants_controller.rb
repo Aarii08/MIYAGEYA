4bff8ba9073cbf805bb79afc7fc64ce2010bea9f
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-10-14 04:56:44 +0000

    [Fix]ほしい！非同期化

--- app/controllers/public/wants_controller.rb
+++ app/controllers/public/wants_controller.rb
@@ -1,17 +1,17 @@
 class Public::WantsController < ApplicationController
 
   def create
-    souvenir = Souvenir.find(params[:souvenir_id])
-    want = current_user.wants.new(souvenir_id: souvenir.id)
+    @souvenir = Souvenir.find(params[:souvenir_id])
+    want = current_user.wants.new(souvenir_id: @souvenir.id)
     want.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    souvenir = Souvenir.find(params[:souvenir_id])
-    want = current_user.wants.find_by(souvenir_id: souvenir.id)
+    @souvenir = Souvenir.find(params[:souvenir_id])
+    want = current_user.wants.find_by(souvenir_id: @souvenir.id)
     want.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
